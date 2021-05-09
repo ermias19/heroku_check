da80086e1f4adab396008fb599416988db10f805
ermias19 <ermiasmulu19@gmail.com>
2021-05-09 10:07:57 +0300

    sadssad

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask, session, render_template ,redirect ,request,url_for, flash, session 
 
 app = Flask(__name__)
-app.config['SECRET_KEY']='5791628bb0b13ce0c676dfde280ba245'
 
-@app.route("/", methods=['GET', 'POST'])
+
+@app.route("/")
 
 def home():
-   return render_template("index.html")
+   return "hi"
 
 if __name__ == '__main__':
-    app.run(debug=True, port="8000")
+    app.run( port="8000")
